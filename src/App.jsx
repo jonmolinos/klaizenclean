fc86077b0019ebaaf09ce6f684a39fcc98474937
jonmolinos <molinosjon@gmail.com>
2026-04-13 22:48:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Klaizen 🚀</h1>
-      <p>Todo funcionando correctamente</p>
+    <div>
+      <h1>Klaizen</h1>
+      <p>Bienestar del aula</p>
     </div>
   );
 }
